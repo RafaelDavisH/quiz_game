1abdd0b9390da7e356819b159e9fa70da661e4f9
RafaelDavisH <thinkprintgrafx@gmail.com>
2017-08-22 13:41:10 -0700

    paragraphs,blanks and quetions updated

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -2,6 +2,7 @@
             LISTS: GREETING FOR LEVELS, BLANKS & PARAGRAPHS
 '''
 
+
 levels = ['Type Start to begin!' + (3*' '),
           'Type Start to begin level2'+ (3*' '),
           'Type Start to begin level3'+ (3*' '),
@@ -14,20 +15,27 @@ levels = ['Type Start to begin!' + (3*' '),
 
 
 blanks = [
-          ["___1___", "___2___", "___3___", "___4___"],
+          ["__________", "__________", "__________"],
+          ["___1___", "___2___", "___3___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___"],
           ["___1___", "___2___", "___3___", "___4___"]
           ]
 
-pg_level = ['''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-tuple, and ___4___ or can be more complicated such as objects and lambda functions.''',
-            '''This is the second paragraph. You specify the inputs a ___1___ takes by
-adding ___2___separated by commas between the parentheses.''']
+pg_level = ['''3 of the 5 ways you can think as a programmers:\n
+  1.__________\n
+  2.__________\n
+  3.__________\n''','''The purpose of ___1___ form is to be able to ___2___ describe exactly the\
+ language in a way thats very simple and very ___3___.''','''The ___1___ name can be ___2___ or ___3___ \n
+___4___ or a mix of all, as long as it starts with a ___5___ or ___6___. ___7___ can vary.''', '''___1___ is \
+a sequence of ___2___ sourrounded by ___3___ - single or ___4___.''', '''An ___1___ refers to position within an \
+ordered list. Python strings can be thought of as list of  ___2___; each ___3___ is given an ___4___ from \
+___5___ to the end of the string. We can use ___6___ numbers which will start ___7___ from the end of the string''']
 
-mult_answers = ['a.Function  b.Perimenters  c.None  or  d.List','a.blah  b.blahs  c.None  or  d.List']
+mult_answers = ['a.Computing Empathy  b.Artificial Thinking  c.Abstract Thinking  d.Technological empathy  e.Procedural Thinking','a.Backus-Naur\
+ b.Congnitive  c.Arithmetically  d.Precisely  e.Concise', ' ', ' ',' ']
 
-
+questions = ['Choose your answer from the following list for ','Choose your answer from the following\
+ list for blank ','Type your answer for the following blank ','Type your answer for the following blank ']
 
 '''
               FUNCTIONS
@@ -53,21 +61,20 @@ def paragraphs(word,blanks):
 
 #         REPLACING BLANKS WITH USERS WORDS
 
-def quiz_game(ml_string, blanks,mult_answers):
+def quiz_game(ml_string, blanks,questions,mult_answers):
   final_pg = []
   ml_string = ml_string.split()
   for word in ml_string:
       replacement = paragraphs(word,blanks)
       if replacement != None:
           print "\n"
-          user_input = raw_input("Choose your answer from the following for blank " + replacement + ": \n" + mult_answers + (3* " "))
+          user_input = raw_input(questions + replacement + ": \n" + mult_answers + (3* " "))
           word = word.replace(replacement, user_input)
           final_pg.append(word)
       else:
           final_pg.append(word)
   final_pg = " ".join(final_pg)
   return "\n" + (3 * " ") + final_pg + "\n\n"
-
 
 
 '''
@@ -89,8 +96,24 @@ print
 
 print greeting(levels[0], start_list,pg_level[0]) + "\n"
 
-print quiz_game(pg_level[0], blanks[0],mult_answers[0])
+print quiz_game(pg_level[0],blanks[0],questions[0],mult_answers[0])
 
-print greeting(levels[1],start_list,pg_level[1])
+print pg_level[1]
 
-print quiz_game(pg_level[1],blanks[1],mult_answers[1])
+print quiz_game(pg_level[1],blanks[1],questions[1],mult_answers[1])
+
+print greeting(levels[1],start_list,pg_level[2]) + "\n"
+
+print quiz_game(pg_level[2],blanks[2],questions[2],mult_answers[2])
+
+print pg_level[3]
+
+print quiz_game(pg_level[3],blanks[3],questions[3],mult_answers[3])
+
+print greeting(levels[2],start_list,pg_level[4]) + "\n"
+
+print quiz_game(pg_level[4],blanks[4],questions[4],mult_answers[4])
+
+print pg_level[5]
+
+print quiz_game(pg_level[5],blanks[5],questions[5],mult_answers[5])
