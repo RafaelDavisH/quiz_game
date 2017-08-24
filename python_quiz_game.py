7534028ba8f72155a7802a6bc6aefa82dd5ac452
RafaelDavisH <thinkprintgrafx@gmail.com>
2017-08-23 22:07:13 -0700

    answer verification and placing the WRONG for each incorrect answer

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -21,9 +21,16 @@ blanks = [
           ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___"]
           ]
 
-answer_key = ['Procedural Thinking','Technological Empathy','Abstract Thinking']
+answer_key = [['procedural thinking','technological empathy','abstract thinking'],
+              ['backus-naur','precisely','concise'],
+              ['variable','letters','numbers','underscore','letter','underscore','variables'],
+              ['string','characters','quotes','double'],
+              ['index','characters','character','index','zero','negative','counting'],
+              ['find','method','target string','position','search string','target string']
+              ]
 
 pg_level = ['''3 of the 5 ways you can think as a programmers:\n
+***The order of your answer matters to get them correctly***\n
   1.__________\n
   2.__________\n
   3.__________\n''','''The purpose of ___1___ form is to be able to ___2___ describe exactly the\
@@ -35,10 +42,10 @@ ___5___ to the end of the string. We can use ___6___ numbers which will start __
 built in Python. When the ___3___ number its given, which gives the first ___4___ in ___5___ where the ___6___ appears. If ___7___ is not \
 found, its given an ___8___.''']
 
-mult_answers = ['a.Computing Empathy  b.Artificial Thinking  c.Abstract Thinking  d.Technological Empathy  e.Procedural Thinking','a.Backus-Naur\
- b.Congnitive  c.Arithmetically  d.Precisely  e.Concise', ' ', ' ',' ',' ']
+mult_choices = ['a.computing empathy  b.artificial thinking  c.abstract thinking  d.technological empathy  e.procedural thinking','a.backus-naur\
+ b.congnitive  c.arithmetically  d.precisely  e.concise', ' ', ' ',' ',' ']
 
-questions = ['Choose your answer from the following list for ','Choose your answer from the following\
+msg_to_follow = ['Choose your answer from the following list for ','Choose your answer from the following\
  list for blank ','Type your answer for the following blank ','Type your answer for the following blank ','Type your answer for the following blank','\
 Type your answer for the following blank ']
 
@@ -61,7 +68,7 @@ def greeting(start_answer, pg_level):
   
 #         PASSING BLANKS IN paragraphs FUNCTION
 
-def paragraphs(word,blanks):
+def paragraphs(word, blanks):
   for pos in blanks:
     if pos in word:
       return pos
@@ -69,23 +76,24 @@ def paragraphs(word,blanks):
 
 #         ANSWER VERIFICATION
 
-def answer_verify(answers,answer_key):
+def answer_verify(answers, answer_key):
   for e in answer_key:
     if e in answers:
       return e
     return None
 
+
 #         REPLACING BLANKS WITH USERS WORDS
 
-def quiz_game(ml_string, blanks,questions,mult_answers):
+def quiz_game(pg_level, blanks, msg_to_follow, mult_choices, answer_key):
   final_pg = []
-  ml_string = ml_string.split()
-  for word in ml_string:
-      replacement = paragraphs(word,blanks)
+  pg_level = pg_level.split()
+  for word in pg_level:
+      replacement = paragraphs(word, blanks)
       if replacement != None:
           print "\n"
-          user_input = raw_input(questions + replacement + ": \n" + mult_answers + (3* " "))
-          checked_answers = answer_verify(user_input,answer_key)
+          user_input = raw_input(msg_to_follow + replacement + ": \n" + mult_choices + (3* " "))
+          checked_answers = answer_verify(user_input, answer_key)
           if checked_answers != None:
             word = word.replace(replacement, checked_answers)
             final_pg.append(word)
@@ -98,18 +106,18 @@ def quiz_game(ml_string, blanks,questions,mult_answers):
   final_pg = " ".join(final_pg)
   return "\n" + (3 * " ") + final_pg + "\n\n"
 
-
+    
 '''
                   GAME START
 '''
 
 print (30 * '-')
-print " "* 4 + "Welcome to my Quiz!"
+print " "* 4 + "Welcome to a Quiz Game!"
 print (30 * '-')
 print
 user = raw_input("Enter your First Name" + (3*' '))
 print
-print 'Hello '+ user + "!"
+print 'Alright '+ user + "here is your chance to check if you know some basic keywords in Python!"
 print
 print "The following is a fill in the blank quiz. You'll be first prompt with \
 paragraphs that will contain numbered blanks. These numbered blanks are keywords \
@@ -118,24 +126,27 @@ print
 
 print greeting(levels[0], pg_level[0])
 
-print quiz_game(pg_level[0], blanks[0], questions[0], mult_answers[0])
+print quiz_game(pg_level[0], blanks[0], msg_to_follow[0], mult_choices[0],answer_key[0])
 
 print pg_level[1]
 
-print quiz_game(pg_level[1], blanks[1], questions[1], mult_answers[1])
+print quiz_game(pg_level[1], blanks[1], msg_to_follow[1], mult_choices[1],answer_key[1])
 
 print greeting(levels[1], pg_level[2]) + "\n"
 
-print quiz_game(pg_level[2], blanks[2], questions[2], mult_answers[2])
+print quiz_game(pg_level[2], blanks[2], msg_to_follow[2], mult_choices[2],answer_key[2])
 
 print pg_level[3]
 
-print quiz_game(pg_level[3], blanks[3], questions[3], mult_answers[3])
+print quiz_game(pg_level[3], blanks[3], msg_to_follow[3], mult_choices[3],answer_key[3])
 
 print greeting(levels[2], pg_level[4]) + "\n"
 
-print quiz_game(pg_level[4], blanks[4], questions[4], mult_answers[4])
+print quiz_game(pg_level[4], blanks[4], msg_to_follow[4], mult_choices[4],answer_key[4])
 
 print pg_level[5]
 
-print quiz_game(pg_level[5], blanks[5], questions[5], mult_answers[5])
+print quiz_game(pg_level[5], blanks[5], msg_to_follow[5], mult_choices[5],answer_key[5])
+
+
+print "GOOD JOB " + user + "! YOU ARE DONE"
