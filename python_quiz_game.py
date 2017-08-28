7a067f7b2c6ce4f01a60b54f8c2e1b021c2c75d9
RafaelDavisH <thinkprintgrafx@gmail.com>
2017-08-28 15:07:14 -0700

    unlimited user chances until correct answer its input & modification in funcitons

--- python_quiz_game.py
+++ python_quiz_game.py
@@ -3,47 +3,48 @@
 '''
 
 
-levels = ['Type "Start" to begin!' + (3*' '),
-          'Type "Start" to begin level2'+ (3*' '),
-          'Type "Start" to begin level3'+ (3*' '),
-          'Type "Start" to begin level4'+ (3*' '),
-          'Type "Start" to begin level5'+ (3*' '),
-          'Type "Start" to begin level6'+ (3*' ')
-          ]
+levels = "Type the level you prefer: easy, medium or hard  "
 
 
-blanks = [
-          ["__________", "__________", "__________"],
-          ["___1___", "___2___", "___3___"],
+easy_blanks = [
+          ["___1___", "___2___", "___3___","___4___", "___5___", "___6___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___", "___9___", "___10___", "___11___"],
           ["___1___","___2___","___3___","___4___","___5___","___6___","___7___"],
-          ["___1___", "___2___", "___3___", "___4___"],
-          ["___1___","___2___","___3___","___4___","___5___","___6___"],
           ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___"]
           ]
 
-answer_key = [['procedural thinking','technological empathy','abstract thinking'],
-              ['backus-naur','precisely','concise'],
-              ['variable','letters','numbers','underscore','letter','underscore','variables'],
-              ['string','characters','quotes','double'],
+medium_blanks = [
+          ["___1___", "___2___", "___3___","___4___", "___5___", "___6___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___", "___9___", "___10___", "___11___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___"]
+          ]
+
+hard_blanks = [
+          ["___1___", "___2___", "___3___","___4___", "___5___", "___6___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___", "___9___", "___10___", "___11___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___"],
+          ["___1___","___2___","___3___","___4___","___5___","___6___","___7___","___8___"]
+          ]
+
+answer_key = [['procedural thinking','technological empathy','abstract thinking','backus-naur','precisely','concise'],
+              ['variable','letters','numbers','underscore','letter','underscore','variables','string','characters','quotes','double'],
               ['index','characters','character','index','zero','negative','counting'],
-              ['find','method','target string','position','search string','target string']
+              ['find','method','target string','position','search string','target string','target string','-1']
               ]
 
 pg_level = ['''3 of the 5 ways you can think as a programmers:\n
-***The order of your answer matters to get them correctly***\n
-  1.__________\n
-  2.__________\n
-  3.__________\n''','''The purpose of ___1___ form is to be able to ___2___ describe exactly the\
- language in a way thats very simple and very ___3___.''','''The ___1___ name can be ___2___ or ___3___ \n
-___4___ or a mix of all, as long as it starts with a ___5___ or ___6___. ___7___ can vary.''', '''___1___ is \
-a sequence of ___2___ surrounded by ___3___ - single or ___4___.''', '''An ___1___ refers to position within an \
+  ___1___ , ___2___ , ___3___ .  The purpose of ___4___ form is to be able to ___5___ describe exactly the\
+ language in a way thats very simple and very ___6___.''','''The ___1___ name can be ___2___ or ___3___ \n
+___4___ or a mix of all, as long as it starts with a ___5___ or ___6___.  ___7___ can vary.  ___8___ is \
+a sequence of ___9___ surrounded by ___10___ - single or ___11___.''', '''An ___1___ refers to position within an \
 ordered list. Python strings can be thought of as list of  ___2___; each ___3___ is given an ___4___ from \
 ___5___ to the end of the string. We can use ___6___ numbers which will start ___7___ from the end of the string''','''___1___ is a ___2___ \
 built in Python. When the ___3___ number its given, which gives the first ___4___ in ___5___ where the ___6___ appears. If ___7___ is not \
-found, its given an ___8___.''']
+found, its given an ___8___.''',' ']
 
-mult_choices = ['a.computing empathy  b.artificial thinking  c.abstract thinking  d.technological empathy  e.procedural thinking','a.backus-naur\
- b.congnitive  c.arithmetically  d.precisely  e.concise', ' ', ' ',' ',' ']
+mult_choices = ['a.computing empathy  b.artificial thinking  c.abstract thinking \n d.technological empathy  e.procedural thinking d.backus-naur\n\
+ e.congnitive  f.arithmetically  g.precisely  h.concise', ' ', ' ',' ',' ']
 
 msg_to_follow = ['Choose your answer from the following list for ','Choose your answer from the following\
  list for blank ','Type your answer for the following blank ','Type your answer for the following blank ','Type your answer for the following blank','\
@@ -56,16 +57,26 @@ Type your answer for the following blank ']
 
 #         GREETING
 
+def play(diff):
+    i = 0
+    while i != 4:
+        print pg_level[i]
+        print quiz_game(pg_level[i], diff[i], msg_to_follow[i], mult_choices[i],answer_key[i])
+        i += 1
+    return " YOU ARE DONE! " + user
+
 def greeting(start_answer, pg_level):
     answer = raw_input(start_answer)
-    if answer == "Start":
-      return "\n" + pg_level
-    if answer == "start":
-      return "\n" + pg_level
+    if answer == "easy":
+      return "\n" + play(easy_blanks)
+    if answer == "medium":
+      return "\n" + play(medium_blanks)
+    if answer == "hard":
+      return "\n" + play(hard_blanks)
     else:
       return greeting(start_answer, pg_level)
-    
-  
+
+
 #         PASSING BLANKS IN paragraphs FUNCTION
 
 def paragraphs(word, blanks):
@@ -77,36 +88,41 @@ def paragraphs(word, blanks):
 #         ANSWER VERIFICATION
 
 def answer_verify(answers, answer_key):
-  for e in answer_key:
-    if e in answers:
-      return e
-    return None
+  print "1st " + answer_key
+  while answers != answer_key:
+    answers = raw_input((11 * ' ') + 'WRONG!!!!!!! TRY AGAIN  ')
+    print "answer varification " + answers
+    if answers == answer_key:
+        return answers
+  return answers
+
 
 
 #         REPLACING BLANKS WITH USERS WORDS
 
 def quiz_game(pg_level, blanks, msg_to_follow, mult_choices, answer_key):
   final_pg = []
+  i = 0
   pg_level = pg_level.split()
   for word in pg_level:
       replacement = paragraphs(word, blanks)
       if replacement != None:
           print "\n"
-          user_input = raw_input(msg_to_follow + replacement + ": \n" + mult_choices + (3* " "))
-          checked_answers = answer_verify(user_input, answer_key)
-          if checked_answers != None:
-            word = word.replace(replacement, checked_answers)
-            final_pg.append(word)
-          else:
-            checked_answers = "WRONG"
-            word = word.replace(replacement, checked_answers)
-            final_pg.append(word)
+          user_input = raw_input(msg_to_follow + replacement + ": \n" + mult_choices + (4* " "))
+          checked_answers = answer_verify(user_input,answer_key[i])
+          i+= 1
+          print "index number " + str(i)
+          word = checked_answers
+          final_pg.append(word)
+#          if checked_answers != None:
+#              word = checked_answers
+#              final_pg.append(word)
       else:
           final_pg.append(word)
   final_pg = " ".join(final_pg)
   return "\n" + (3 * " ") + final_pg + "\n\n"
 
-    
+
 '''
                   GAME START
 '''
@@ -124,29 +140,6 @@ paragraphs that will contain numbered blanks. These numbered blanks are keywords
 from 'Introduction to Serious Programming'."
 print
 
-print greeting(levels[0], pg_level[0])
 
-print quiz_game(pg_level[0], blanks[0], msg_to_follow[0], mult_choices[0],answer_key[0])
+print greeting(levels,pg_level)
 
-print pg_level[1]
-
-print quiz_game(pg_level[1], blanks[1], msg_to_follow[1], mult_choices[1],answer_key[1])
-
-print greeting(levels[1], pg_level[2]) + "\n"
-
-print quiz_game(pg_level[2], blanks[2], msg_to_follow[2], mult_choices[2],answer_key[2])
-
-print pg_level[3]
-
-print quiz_game(pg_level[3], blanks[3], msg_to_follow[3], mult_choices[3],answer_key[3])
-
-print greeting(levels[2], pg_level[4]) + "\n"
-
-print quiz_game(pg_level[4], blanks[4], msg_to_follow[4], mult_choices[4],answer_key[4])
-
-print pg_level[5]
-
-print quiz_game(pg_level[5], blanks[5], msg_to_follow[5], mult_choices[5],answer_key[5])
-
-
-print "GOOD JOB " + user + "! YOU ARE DONE"
